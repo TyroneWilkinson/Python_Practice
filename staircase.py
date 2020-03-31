bc4ececcad5a7cd2855f6faf2683fcbdf8054108
Tyrone Wilkinson <tyronerwilkinson@gmail.com>
2020-03-31 08:28:38 -0400

    a program that prints a right-aligned staircase

--- staircase.py
+++ staircase.py
@@ -6,10 +6,19 @@ import random
 import re
 import sys
 
-# Complete the staircase function below.
 def staircase(n):
+	"""
+	Prints a right-aligned staircase of size n using # symbols and spaces.
+	
+	Parameter:
+	n (integer): Denotes the size of the staircase.	
+	"""
+	i = 1
+	while i < n+1:
+		print(("#"*i).rjust(n))
+		i += 1
 
 if __name__ == '__main__':
     n = int(input())
-
+		
     staircase(n)
