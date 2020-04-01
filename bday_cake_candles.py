6c3cac3a7300f9ad5f5181d8171e6a1584992850
Tyrone Wilkinson <tyronerwilkinson@gmail.com>
2020-04-01 12:11:39 -0400

    A program that determines the quantity of the largest integer(s) in the list

---
+++ bday_cake_candles.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def birthdayCakeCandles(ar):
+	"""
+	Determines the quantity of the largest integer(s) in the list.
+	
+	Parameter:
+	ar (list): A list of integers representing candle heights.
+
+	Return:
+	integer: An integer representing the number of candles that can be
+					 blown.	
+	"""
+	return ar.count(max(ar))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
